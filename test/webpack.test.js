0600a9c38bd496e95580340a31d5febb672fcdf1
Magnus Brzenk <magnusbrzenk@gmail.com>
2018-08-23 14:22:23 -0400

    Bookmark

---
+++ test/webpack.test.js
@@ -0,0 +1,17 @@
+// webpack.config.js
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /test\.js$/,
+                use: "mocha-loader",
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
